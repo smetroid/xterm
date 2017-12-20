0771e0b4648c8771072aa6cd0f85d064c7f5a36c
EC <enriquegc1982@gmail.com>
2017-12-19 21:12:41 -0600

    cleaning up system shortcuts, adding xfce configs

--- dependencies.sh
+++
@@ -1,6 +0,0 @@
-sudo yum install ctags-etags
-sudo yum install cmake
-#sudo yum -y install qt-devel cmake
-sudo yum install gcc-c++
-sudo yum install xfce4-terminal
-sudo yum install vim-X11
--- ycm-pre-install.sh
+++
@@ -1,4 +0,0 @@
-git submodule update --init --recursive
-./install.sh --clang-completer
-
-
